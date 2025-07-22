0db62b79ac33c90f19d38773a4463b255597bb50
Damian Kmiecik <damiankmiecik@protonmail.com>
2025-07-22 19:44:30 +0200

    Minor refactoring for visuals while running automated tests

---
+++ page_objects/contact_page.py
@@ -0,0 +1,39 @@
+from playwright.sync_api import Page, Locator
+from page_objects.components.header_component import HeaderComponent
+from page_objects.components.footer_component import FooterComponent
+
+class ContactPage:
+    """Represents the contact page of the application"""
+    def __init__(self, page:Page):
+
+        self.page = page
+
+        # Contact page contains header and footer components
+        self.header = HeaderComponent(page)
+        self.footer = FooterComponent(page)
+
+        # Locators for the contact page elements
+        self.title = page.locator('[data-testid="title"]')
+        self.title_photo = page.locator('[data-testid="title-photo"]')
+        self.title_redirect = page.locator('[data-testid="redirect"]')
+        self.contact_form = page.locator('[data-testid="contact-form"]')
+        self.form_details = page.locator('[data-testid="form-details"]')
+        self.city = page.locator('[data-testid="city"]')
+        self.address = page.locator('[data-testid="address"]')
+        self.address_details = page.locator('[data-testid="address-details"]')
+        self.phone = page.locator('[data-testid="phone"]')
+        self.phone_1 = page.locator('[data-testid="phone-1"]')
+        self.phone_2 = page.locator('[data-testid="phone-2"]')
+        self.email = page.locator('[data-testid="email"]')
+        self.email_address = page.locator('[data-testid="email-address"]')
+        self.photo = page.locator('[data-testid="photo"]')
+
+    def navigate(self):
+        self.page.goto("kontakt/", wait_until="domcontentloaded")
+
+
+
+
+
+
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,28 @@
 import pytest
 from playwright.sync_api import Page
+from playwright.sync_api import Browser, BrowserContext, Page
 from page_objects.home_page import HomePage
+from page_objects.contact_page import ContactPage
+
+try:
+    import tkinter
+    root = tkinter.Tk()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    root.destroy()
+    SCREEN_SIZE = {"width": screen_width, "geight": screen_height}
+except (ImportError, RuntimeError):
+    SCREEN_SIZE = {"width": 1920, "height": 1080}  # Default screen size if tkinter is not available
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    """Fixture to configure browser context arguments for all tests."""
+    return {
+        **browser_context_args,
+        "viewport": SCREEN_SIZE,
+        "locale": "en-US", # Set the locale to English
+        "ignore_https_errors": True # Ignore SSL errors
+    }
 
 @pytest.fixture
 def home_page(page: Page) -> HomePage:
@@ -8,3 +30,10 @@ def home_page(page: Page) -> HomePage:
     home_page = HomePage(page)
     home_page.navigate()
     return home_page
+
+@pytest.fixture
+def contact_page(page: Page) -> ContactPage:
+    """Fixture to initialize ContactPage object and navigate to it."""
+    contact_page = ContactPage(page)
+    contact_page.navigate()
+    return contact_page
